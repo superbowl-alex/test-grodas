f1bc92d0ea31d616a074ef1f2d52a476361eabfc
Oleksandr <superbowl@ukr.net>
2022-12-31 14:48:53 +0200

    Test changes

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ let storage = [];
 
 const getImages = async () => {
   const response = await fetch("../db/data.json");
+  console.log(response);
   if (!response.ok) {
     throw new Error(response.status);
   }
